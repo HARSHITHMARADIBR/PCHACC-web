20c46341ffd32159171d49618642a4f08f6ed686
HARSHITHMARADIBR <harshithmaradi@gmail.com>
2025-07-25 00:32:09 +0530

    Update User.py

--- User.py
+++ User.py
@@ -1,41 +1,39 @@
 import streamlit as st
-
 from Home_page import home_process
 from CTA_Fort import Chitradurga_fort
 from Chandravalli import chandravalli_details
 from Rangayyana import rangayyana_baagilu
 from uchanngi import uchhangellamma_temple
 from maramma import maramma_temple
-from barageramma import barageramma_temple
+from barageramma import barageramma_temple  # Fixed import
 from muruga import murugha_mata
 from Study_area import study_area
 from proposal import proposal
 
 def user_process():
-    # Initialize session state
     if "page" not in st.session_state:
         st.session_state.page = "home"
 
-    # Routing
-    if st.session_state.page == "home":
+    page = st.session_state.page
+    if page == "home":
         home_process()
-    elif st.session_state.page == "Cta_fort":
+    elif page == "Cta_fort":
         Chitradurga_fort()
-    elif st.session_state.page == "Chandravalli":
+    elif page == "Chandravalli":
         chandravalli_details()
-    elif st.session_state.page == "Rangayyana":
+    elif page == "Rangayyana":
         rangayyana_baagilu()
-    elif st.session_state.page == "Uchhangellamma":
+    elif page == "Uchhangellamma":
         uchhangellamma_temple()
-    elif st.session_state.page == "Maramma":
+    elif page == "Maramma":
         maramma_temple()
-    elif st.session_state.page == "Barageramma":
-        barageramma_temple()
-    elif st.session_state.page == "Murugha":
+    elif page == "Barageramma":
+        barageramma_temple()  # Now correctly mapped
+    elif page == "Murugha":
         murugha_mata()
-    elif st.session_state.page == "study_area":
+    elif page == "study_area":
         study_area()
-    elif st.session_state.page == "proposal":
+    elif page == "proposal":
         proposal()
     else:
         st.warning("Page not found!")
