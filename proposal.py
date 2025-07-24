d07e9c5c27c54bf57d01bdc77f27f78ff01c0d20
HARSHITHMARADIBR <harshithmaradi@gmail.com>
2025-07-24 23:42:35 +0530

    Update proposal.py

--- proposal.py
+++ proposal.py
@@ -52,7 +52,7 @@ def proposal():
     )
     st.markdown("***Proposed Rehabilitation Site ***")
 
-    Pro2 =r"C:\Users\harshith maradi b r\Documents\Downloads\Prohibited\web streamlit\personal finance tracker\personal finance tracker\images\IMG-20250703-WA0049.jpg"
+    Pro2 =images/IMG-20250703-WA0049.jpg
     with open(Pro2, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
     st.markdown(
