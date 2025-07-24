a6b6ad30ff75b9b8b8cded7f0bebf5303467e50c
HARSHITHMARADIBR <harshithmaradi@gmail.com>
2025-07-24 23:36:06 +0530

    Update maramma.py

--- maramma.py
+++ maramma.py
@@ -54,7 +54,7 @@ def maramma_temple():
     
     st.markdown("***Factors affecting in Maramma Temple***")
 
-    p1 = r"C:\Users\harshith maradi b r\Documents\Downloads\Prohibited\web streamlit\personal finance tracker\personal finance tracker\images\IMG-20250703-WA0036.jpg"
+    p1 = images/IMG-20250703-WA0036.jpg
     p2 = r"C:\Users\harshith maradi b r\Documents\Downloads\Prohibited\web streamlit\personal finance tracker\personal finance tracker\images\IMG-20250703-WA0025.jpg"
 
     # Create 3 columns
