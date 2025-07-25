f907c890fb0331942fab2ac924cb7ff13cf95d47
HARSHITHMARADIBR <harshithmaradi@gmail.com>
2025-07-25 10:19:32 +0530

    Update proposal.py

--- proposal.py
+++ proposal.py
@@ -7,6 +7,7 @@ def proposal():
         st.rerun()
         
     st.title("PROPOSALS")
+
     st.write('''GUIDELINES: As and when any unauthorized construction is reported in prohibited or regulated area, ASI initiates action as per provisions of the Ancient Monuments and Archaeological Sites and Remains Act, 1958 (AMASR Act, 1958).''')
     st.write('''As per the existing rule position, [section 20A of the AMASR Act, 1958, stipulates that an area extending to 100 meters from the protected limit is prohibited area] and [section 20B of the AMASR Act, 1958 stipulate that an area extending to 200 meters from the prohibited limit is regulated area].''')
     st.write("""
@@ -39,119 +40,84 @@ def proposal():
     - The land under government possession near **Ingaldalu Village** beside the National Highway (Survey No: 79, **128.91538041 hectares**) will be used to **relocate people** currently living in **prohibited heritage zones** in Chitradurga City.
     """)
 
-    Pro1 = images/IMG-20250703-WA0040.jpg
+    # --- Image 1 ---
+    Pro1 = "images/IMG-20250703-WA0040.jpg"
     with open(Pro1, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-        f"""
-        <h1 style="display: flex; align-items: center;">
-            <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="500" style="margin-right:50px;">
-        </h1>
-        """,
-        unsafe_allow_html=True
-    )
-    st.markdown("***Proposed Rehabilitation Site ***")
+    st.markdown(f"""
+        <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="500">
+    """, unsafe_allow_html=True)
+    st.markdown("***Proposed Rehabilitation Site***")
 
-    Pro2 =images/IMG-20250703-WA0049.jpg
+    # --- Image 2 ---
+    Pro2 = "images/IMG-20250703-WA0049.jpg"
     with open(Pro2, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-        f"""
-        <h1 style="display: flex; align-items: center;">
-            <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="500" style="margin-right:50px;">
-        </h1>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown(f"""
+        <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="500">
+    """, unsafe_allow_html=True)
     st.markdown("***Major locations from Proposed Site***")
-    
-    Pro3 =images/IMG-20250703-WA0012.jpg
+
+    # --- Image 3 ---
+    Pro3 = "images/IMG-20250703-WA0012.jpg"
     with open(Pro3, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-        f"""
-        <h1 style="display: flex; align-items: center;">
-            <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="400" style="margin-right:50px;">
-        </h1>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown(f"""
+        <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="400">
+    """, unsafe_allow_html=True)
     st.markdown("***Details of Encroachment***")
 
+    # --- Site Analysis ---
     st.subheader("SITE ANALYSIS OF LAYOUT")
     st.write("""
     - Total Area: 128.91538041 Hectors
-             
     - Coordinates: 14°11'11.5"N 76°27'26.3"E or 5FP4+JW8 Ingaladal, Karnataka 
-             
     - Connectivity: Banglore-Mumbai National Highway passes Infront the site area, 8.7Km from Chitradurga govt Bus stand, 12.1Km from Railway Station
-             
     - Climatic Condition: Temperature is usually normal, Wind direction is from North-East to South-West, Sun Path follows East to West       
-             
     - Vegetation: Vegetation can be seen on the north and south Western and South Western region
     """)
 
-    Pro4 =images/Ingaldallayoutdesign.jpg
+    # --- Image 4 ---
+    Pro4 = "images/Ingaldallayoutdesign.jpg"
     with open(Pro4, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-        f"""
-        <h1 style="display: flex; align-items: center;">
-            <img src="data:image/jpeg;base64,{encoded_image}" width="600" height="800" style="margin-right:50px;">
-        </h1>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown(f"""
+        <img src="data:image/jpeg;base64,{encoded_image}" width="600" height="800">
+    """, unsafe_allow_html=True)
     st.markdown("***Layout Design of 79 Survey number***")
 
-    Pro5 =images/Screenshot 2025-07-22 201450.png
+    # --- Image 5 ---
+    Pro5 = "images/Screenshot 2025-07-22 201450.png"
     with open(Pro5, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-        f"""
-        <h1 style="display: flex; align-items: center;">
-            <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="400" style="margin-right:50px;">
-        </h1>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown(f"""
+        <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="400">
+    """, unsafe_allow_html=True)
     st.markdown("***Landuse Analysis***")
 
-    Pro6 =images/landuse Analysis chart.png
+    # --- Image 6 ---
+    Pro6 = "images/landuse Analysis chart.png"
     with open(Pro6, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-        f"""
-        <h1 style="display: flex; align-items: center;">
-            <img src="data:image/jpeg;base64,{encoded_image}" width="600" height="500" style="margin-right:50px;">
-        </h1>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown(f"""
+        <img src="data:image/jpeg;base64,{encoded_image}" width="600" height="500">
+    """, unsafe_allow_html=True)
     st.markdown("***Landuse Analysis chart***")
 
-    Pro7 =images/plot analysis table.png
+    # --- Image 7 ---
+    Pro7 = "images/plot analysis table.png"
     with open(Pro7, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-        f"""
-        <h1 style="display: flex; align-items: center;">
-            <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="450" style="margin-right:50px;">
-        </h1>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown(f"""
+        <img src="data:image/jpeg;base64,{encoded_image}" width="700" height="450">
+    """, unsafe_allow_html=True)
     st.markdown("***Plot Analysis***")
 
-    Pro8 =images/Plot Analysis chart.png
+    # --- Image 8 ---
+    Pro8 = "images/Plot Analysis chart.png"
     with open(Pro8, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
-    st.markdown(
-        f"""
-        <h1 style="display: flex; align-items: center;">
-            <img src="data:image/jpeg;base64,{encoded_image}" width="600" height="550" style="margin-right:50px;">
-        </h1>
-        """,
-        unsafe_allow_html=True
-    )
+    st.markdown(f"""
+        <img src="data:image/jpeg;base64,{encoded_image}" width="600" height="550">
+    """, unsafe_allow_html=True)
     st.markdown("***Plot Analysis chart***")
