f9724a1cc2cc159a3e0527bf1e8eb0d60f00ea1b
HARSHITHMARADIBR <harshithmaradi@gmail.com>
2025-07-25 11:14:06 +0530

    Update muruga.py

--- muruga.py
+++ muruga.py
@@ -18,9 +18,30 @@ def murugha_mata():
     st.write("**Coordinates:** 14.2300° N, 76.4000° E")
 
     # Display main image
-    img = "images/murugha_main.jpg"
+    img = "images/IMG-20250703-WA0015.jpg"
     try:
         with open(img, "rb") as f:
+            encoded = base64.b64encode(f.read()).decode()
+        st.markdown(
+            f'<img src="data:image/jpeg;base64,{encoded}" width="500">',
+            unsafe_allow_html=True
+        )
+    except FileNotFoundError:
+        st.warning(f"Image not found: {img}")
+    img2 = "images/IMG-20250703-WA0030.jpg"
+    try:
+        with open(im2, "rb") as f:
+            encoded = base64.b64encode(f.read()).decode()
+        st.markdown(
+            f'<img src="data:image/jpeg;base64,{encoded}" width="500">',
+            unsafe_allow_html=True
+        )
+    except FileNotFoundError:
+        st.warning(f"Image not found: {img}")
+
+    img3 = "images/IMG-20250703-WA0033.jpg"
+    try:
+        with open(img3, "rb") as f:
             encoded = base64.b64encode(f.read()).decode()
         st.markdown(
             f'<img src="data:image/jpeg;base64,{encoded}" width="500">',
